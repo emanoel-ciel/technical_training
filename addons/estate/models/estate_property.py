6d193ba083acfab720722423e20f5c7839aa7017
emanoel-ciel <emanoel.cardoso@ciel-it.com>
2024-10-03 17:48:51 -0300

    feat(estate): Adicionar campos e métodos ao modelo de propriedade
    Adiciona campos e métodos ao modelo de propriedade para melhorar a funcionalidade e visualização das propriedades imobiliárias.
    
    - Adiciona os campos 'active' e 'state' ao modelo de propriedade.
    - Atualiza o campo 'date_availability' para ser calculado automaticamente.
    - Atualiza os campos 'selling_price' e 'bedrooms' com novas configurações.
    - Adiciona os arquivos 'demo.xml' e 'res_groups.xml' ao módulo de propriedades.
    - Atualiza as visualizações do modelo de propriedade para exibir os campos relevantes.
    
    Closes #123

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,4 +1,7 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from logging import getLogger
+
+__logger = getLogger(__name__)
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -7,10 +10,10 @@ class EstateProperty(models.Model):
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Available From')
+    date_availability = fields.Date(string='Available From', copy=False, compute='_compute_date_availability', store=True)
     expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
@@ -19,3 +22,23 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         string='Garden Orientation')
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection(
+        [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'),
+         ('sold', 'Sold'), ('canceled', 'Canceled')],
+        string='Status', default='new')
+
+
+
+
+
+    @api.onchange('date_availability')
+    def _compute_date_availability(self):
+        toDay = fields.Date.today()
+        three_months_later = fields.Date.add(toDay, months=3)
+
+        __logger.info(f"***ATENÇÃO***: Dia atual é {toDay}, trê meses depois é {three_months_later}")
+
+        for record in self:
+            if record.date_availability:
+                record.date_availability = three_months_later
