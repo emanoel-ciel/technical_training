1605d276736b7e70a0e8903e2ea7f9035c638cdf
emanoel-ciel <emanoel.cardoso@ciel-it.com>
2024-10-17 15:40:38 -0300

    Add(estate_property_offer): Adicionado novo modelo para registrar as ofertas de propriedades imobiliárias.

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,56 +1,102 @@
 from odoo import models, fields, api
-
+import datetime
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
 
-    name = fields.Char(string='Title', required=True)
-    description = fields.Text(string='Description')
-    postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Available From', copy=False, compute='_compute_date_availability', store=True)
-    expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
-    bedrooms = fields.Integer(string='Bedrooms', default=2)
-    living_area = fields.Integer(string='Living Area')
-    facades = fields.Integer(string='Facades')
-    garage = fields.Boolean(string='Garage')
-    garden = fields.Boolean(string='Garden')
-    garden_area = fields.Integer(string='Garden Area')
+    ##########
+    # FIELDS #
+    ##########
+
+    name = fields.Char(
+        string='Title', 
+        required=True)
+    
+    description = fields.Text(
+        string='Description')
+    
+    postcode = fields.Char(
+        string='Postcode')
+    
+    date_availability = fields.Date(
+        string='Available From', 
+        copy=False, 
+        compute='_compute_date_availability', 
+        store=True)
+    
+    expected_price = fields.Float(
+        string='Expected Price', 
+        required=True)
+    
+    selling_price = fields.Float(
+        string='Selling Price', 
+        readonly=True, copy=False)
+    
+    bedrooms = fields.Integer(
+        string='Bedrooms', 
+        default=2)
+    
+    living_area = fields.Integer(
+        string='Living Area')
+    
+    facades = fields.Integer(
+        string='Facades')
+    
+    garage = fields.Boolean(
+        string='Garage')
+    
+    garden = fields.Boolean(
+        string='Garden')
+    
+    garden_area = fields.Integer(
+        string='Garden Area')
+    
     garden_orientation = fields.Selection(
         [('north', 'North'), 
          ('south', 'South'), 
          ('east', 'East'), 
-         ('west', 'West')], string='Garden Orientation')
-    active = fields.Boolean(string='Active', default=True)
+         ('west', 'West')], 
+         string='Garden Orientation')
+    
+    active = fields.Boolean(
+        string='Active', 
+        default=True)
+    
     state = fields.Selection(
         [('new', 'New'), 
          ('offer_received', 'Offer Received'), 
          ('offer_accepted', 'Offer Accepted'),
          ('sold', 'Sold'), 
-         ('canceled', 'Canceled')], string='Status', default='new')
-    estate_property_type_id = fields.Many2one('estate.property.type', string='Property Type')
-    buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
-    salesperson_id = fields.Many2one('res.users', string='Salesperson')
-    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers', required=True)
+         ('canceled', 'Canceled')], 
+         string='Status', 
+         default='new')
+    
+    estate_property_type_id = fields.Many2one(
+        comodel_name = 'estate.property.type', 
+        string='Property Type')
+    
+    buyer_id = fields.Many2one(
+        comodel_name = 'res.partner', 
+        string='Buyer', 
+        copy=False)
+    
+    salesperson_id = fields.Many2one(
+        comodel_name = 'res.users', 
+        string='Salesperson')
+    
+    offer_ids = fields.One2many(
+        comodel_name = 'estate.property.offer', 
+        inverse_name='property_id', 
+        string='Offers')
 
-    @api.onchange('date_availability')
+    ###########
+    # METHODS #
+    ###########
+
     def _compute_date_availability(self):
-        toDay = fields.Date.today()
-        three_months_later = fields.Date.add(toDay, months=3)
-
+        value = datetime.date.today() + datetime.timedelta(days=90)
         for record in self:
             if record.date_availability:
-                record.date_availability = three_months_later
-
-
-class EstatePropertyOffer(models.Model):
-    _name = 'estate.property.offer'
-    _description = 'Real Estate Property Offer'
-
-    price = fields.Float(string='Price')
-    status = fields.Selection(
-        [('accepted', 'Accepted'), 
-         ('refused', 'Refused')], string='Status', copy=False)
-    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
-    property_id = fields.Many2one('estate.property', string='Property', required=True)
+                # Aproximadamente 3 meses a partir da data atual.
+                record.write({'date_availability': value})
---
+++ addons/estate/models/estate_property_offer.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, api
+
+class EstatePropertyOffer(models.Model):
+    _name = 'estate.property.offer'
+    _description = 'Real Estate Property Offer'
+
+    price = fields.Float(
+        string='Price')
+    
+    status = fields.Selection(
+        [('accepted', 'Accepted'), 
+         ('refused', 'Refused')], 
+         string='Status', 
+         copy=False)
+    
+    partner_id = fields.Many2one(
+        comodel_name = 'res.partner', 
+        string='Partner', 
+        required=True)
+    
+    property_id = fields.Many2one(
+        comodel_name = 'estate.property', 
+        string='Property', 
+        required=True)
